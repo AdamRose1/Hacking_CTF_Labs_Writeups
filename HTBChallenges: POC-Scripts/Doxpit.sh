45d7508c176bd37b677bbec35297c4a2b6ab059b
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2024-08-06 13:50:30 -0400

    Create Doxpit.sh

---
+++ HTBChallenges: POC-Scripts/Doxpit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+"""
+Created this script to solve the HackTheBox challenge called 'DoxPit'.  
+# This script exploits a server side request forgery that is then leveraged to exploit a server side
+template injection that is only accessible using the server side request forgery.
+"""
+
